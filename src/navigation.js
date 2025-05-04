5a1d7891d8ecc286f0faa978046342d1b05bde51
Andres Lopez <andresfelipelopezvasquez@gmail.com>
2025-05-03 20:43:45 -0500

    Navigation page agregada

---
+++ src/navigation.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded', navigator, false)
+window.addEventListener('hashchange', navigator, false);
+
+function navigator() {
+    console.log({location});
+
+    if (location.hash.startsWith('#trends')) {
+        trendsPage();
+    } else if(location.hash.startsWith('#search=')) {
+        searchPage();
+    } else if(location.hash.startsWith('#movie=')) {
+        moviesPage();
+    } else if(location.hash.startsWith('#category=')) {
+        categoriesPage();
+    } else {
+        homePage();
+    }
+};
+
+function homePage() {
+    console.log('Home!!');
+    getCategoriesPreview();
+    getTrendingMoviesPreview();
+}
+
+function categoriesPage() {
+    console.log('Category!!');
+}
+
+function moviesPage() {
+    console.log('Movies!');
+}
+
+function searchPage() {
+    console.log('Search!');
+}
+
+function trendsPage() {
+    console.log('TRENDS!');
+}
