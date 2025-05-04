c118a1752f2f51022e489ba37769767a6861bb65
Andres Lopez <andresfelipelopezvasquez@gmail.com>
2025-05-04 11:57:46 -0500

    Se le coloco un limite al Scrolling de la Trending Section

--- src/main.js
+++ src/main.js
@@ -125,6 +125,7 @@ async function getMoviesBySearch (query) {
 async function getTrendingMovies () {
     const {data} = await api('trending/movie/day');
     const movies = data.results;
+    maxPage = data.total_pages;
 
     createMovies(movies, genericSection, {lazyLoad: true, clean: true});
 
@@ -141,7 +142,9 @@ async function getPaginatedTrendingMovies() {
 
     const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
     const scrollIsBottom = (scrollTop + clientHeight) >= (scrollHeight -15);
-    if (scrollIsBottom) {
+    const pageIsNotMax = page < maxPage;
+    
+    if (scrollIsBottom && pageIsNotMax) {
         page++
     const {data} = await api('trending/movie/day',  {
         params: {
